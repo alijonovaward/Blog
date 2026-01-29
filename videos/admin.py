12c36e8ab90fef6f2c227988822c3b6807361ffe
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-01-29 15:10:01 +0500

    course field boyicha search qilish mumkin

--- videos/admin.py
+++ videos/admin.py
@@ -4,8 +4,8 @@ from .models import VideoLesson, Tag, Course
 @admin.register(VideoLesson)
 class VideoLessonAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "created_at")
-    list_filter = ("author", "tags")
-    search_fields = ("title", "description")
+    list_filter = ("author", "tags", "course")
+    search_fields = ("title", "description", "course")
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
