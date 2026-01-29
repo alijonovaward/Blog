728228b79e61479e1a871659c355ed9c38b48531
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-01-29 16:05:38 +0500

    maqolada ham commentariya korinadi

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,7 @@
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render, get_object_or_404
+
+from comments.models import Comment
 from .models import Article
 
 def article_list(request):
@@ -7,4 +10,15 @@ def article_list(request):
 
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
-    return render(request, "articles/article_detail.html", {"article": article})
+
+    content_type = ContentType.objects.get_for_model(Article)
+    comments = Comment.objects.filter(
+        content_type=content_type,
+        object_id=article.id
+    ).select_related('user').order_by('-created_at')
+
+    context = {
+        "article": article,
+        "comments": comments,
+    }
+    return render(request, "articles/article_detail.html", context)
