f0785e0553336fdcc9af71e98e2e2e94fbfbb1fb
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-01-29 15:30:59 +0500

    a

--- videos/urls.py
+++ videos/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import video_list, video_detail
+from .views import video_list, video_detail, course_detail
 
 urlpatterns = [
     path("", video_list, name="video_list"),
     path("<int:pk>/", video_detail, name="video_detail"),
-    path("course/<int:pk>/", video_list, name="course_list"),
+    path("course/<int:pk>/", course_detail, name="course_list"),
 ]
