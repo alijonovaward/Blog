f55c6995ecef6cc97e9f1dcf2b9a7b270aafa316
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-01-29 15:45:37 +0500

    home menu dan ham stan alone bolmagan videolar olindi

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 def home(request):
     # Oxirgi 5 ta article va video
     latest_articles = Article.objects.all().order_by("-created_at")[:5]
-    latest_videos = VideoLesson.objects.all().order_by("-created_at")[:5]
+    latest_videos = VideoLesson.objects.filter(is_standalone=True).order_by("-created_at")[:5]
     return render(request, "core/home.html", {
         "latest_articles": latest_articles,
         "latest_videos": latest_videos,
