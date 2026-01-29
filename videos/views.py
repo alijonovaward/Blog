c90dc9d78da108f7a59d5d6e1d57694bd3a5911c
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-01-29 11:47:59 +0500

    video dars ichida kommentraiya tuzatildi

--- videos/views.py
+++ videos/views.py
@@ -5,6 +5,18 @@ def video_list(request):
     videos = VideoLesson.objects.all().order_by("-created_at")
     return render(request, "videos/video_list.html", {"videos": videos})
 
+
 def video_detail(request, pk):
     video = get_object_or_404(VideoLesson, pk=pk)
-    return render(request, "videos/video_detail.html", {"video": video})
+
+    # Kommentlarni olish
+    content_type = ContentType.objects.get_for_model(VideoLesson)
+    comments = Comment.objects.filter(
+        content_type=content_type,
+        object_id=video.id
+    ).select_related('user').order_by('-created_at')
+
+    return render(request, "videos/video_detail.html", {
+        "video": video,
+        "comments": comments
+    })
